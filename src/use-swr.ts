0a3ce2a04c0bced0c80bd98c5baf4bf057f28319
Shu Ding <ds303077135@gmail.com>
2019-10-31 07:26:10 +0800

    Fix suspense hanging (#49)
    * fix suspense on hydration
    
    * Update src/use-swr.ts

--- src/use-swr.ts
+++ src/use-swr.ts
@@ -116,7 +116,11 @@ function useSWR<Data = any, Error = any>(
   }
 
   // stale: get from cache
-  let [data, setData] = useState(useHydration() ? undefined : cacheGet(key))
+  let [data, setData] = useState(
+    config.suspense ? cacheGet(key) : useHydration() ? undefined : cacheGet(key)
+  )
+  // it is fine to call `useHydration` conditionally here
+  // because `config.suspense` should never change
   let [error, setError] = useState()
   let [isValidating, setIsValidating] = useState(false)
 
@@ -335,7 +339,7 @@ function useSWR<Data = any, Error = any>(
   }, [key, config.refreshInterval, revalidate])
 
   // suspense (client side only)
-  if (config.suspense && !data) {
+  if (config.suspense && typeof data === 'undefined') {
     if (typeof window !== 'undefined') {
       if (!CONCURRENT_PROMISES[key]) {
         // need to trigger revalidate immediately
