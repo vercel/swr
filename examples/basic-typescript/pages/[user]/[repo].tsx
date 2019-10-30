a754271bfb6088218e0aa14d43f56ca071de4958
Marcel Mokoš <marcelmokos@users.noreply.github.com>
2019-10-30 17:37:32 +0100

    feature/basic-typescript-example: add basic-typescript example (#36)

---
+++ examples/basic-typescript/pages/[user]/[repo].tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'next/link'
+import fetch from '../../libs/fetch'
+
+import useSWR from '@zeit/swr'
+
+export default () => {
+  const id =
+    typeof window !== 'undefined' ? window.location.pathname.slice(1) : ''
+  const { data } = useSWR<{
+    forks_count: number
+    stargazers_count: number
+    watchers: number
+  }>('/api/data?id=' + id, fetch)
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>{id}</h1>
+      {data ? (
+        <div>
+          <p>forks: {data.forks_count}</p>
+          <p>stars: {data.stargazers_count}</p>
+          <p>watchers: {data.watchers}</p>
+        </div>
+      ) : (
+        'loading...'
+      )}
+      <br />
+      <br />
+      <Link href="/">
+        <a>Back</a>
+      </Link>
+    </div>
+  )
+}
