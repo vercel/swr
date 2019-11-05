81cf10a6ce7caa9f962ebfb454e027e8271e2f5d
afc163 <afc163@gmail.com>
2019-11-05 20:15:07 +0800

    Add local state sharing example

---
+++ examples/local-state-sharing/pages/index.js
@@ -0,0 +1,52 @@
+import { useState } from "react"
+import initialStore from "../libs/store"
+import useSWR, { mutate } from "swr"
+
+function Profile() {
+  const { data } = useSWR("globalState", () => Promise.resolve(initialStore))
+  const [value, updateValue] = useState((data || {}).name)
+  if (!data) {
+    return null
+  }
+  return (
+    <div>
+      <h1>My name is {data.name}.</h1>
+      <input
+        value={value}
+        onChange={e => updateValue(e.target.value)}
+        style={{ width: 200, marginRight: 8 }}
+      />
+      <button
+        type="button"
+        onClick={() => {
+          mutate("globalState", { ...data, name: value })
+        }}
+      >
+        Uppercase my name!
+      </button>
+    </div>
+  )
+}
+
+function Other() {
+  const { data } = useSWR("globalState", () => Promise.resolve(initialStore))
+  if (!data) {
+    return null
+  }
+  return (
+    <div style={{ border: "1px solid #ddd", marginTop: 30, padding: 20 }}>
+      <h1>
+        Another Component: <br />
+        My name is {data.name}.
+      </h1>
+    </div>
+  )
+}
+
+export default () => (
+  <div style={{ padding: 40 }}>
+    useSWR can share state between components:
+    <Profile />
+    <Other />
+  </div>
+)
