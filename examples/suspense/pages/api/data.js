30193060b2060ee6f58c90eee235324b82d1f55c
Wendell <wendzhue@gmail.com>
2019-11-11 03:50:07 +0800

    demo: suspense demo (#105)
    * demo: suspense demo
    
    * chore: change dependency version and remove lockfile
    
    * chore: update dependencies

---
+++ examples/suspense/pages/api/data.js
@@ -0,0 +1,34 @@
+import fetch from 'isomorphic-unfetch'
+
+const projects = [
+  'facebook/flipper',
+  'vuejs/vuepress',
+  'rust-lang/rust',
+  'zeit/next.js',
+  'emperor/clothes'
+]
+
+export default (req, res) => {
+  if (req.query.id) {
+    if (req.query.id === projects[4]) {
+      setTimeout(() => {
+        res.json({ msg: 'not found' })
+      })
+
+      return
+    }
+    // a slow endpoint for getting repo data
+    fetch(`https://api.github.com/repos/${req.query.id}`)
+      .then(res => res.json())
+      .then(data => {
+        setTimeout(() => {
+          res.json(data)
+        }, 2000)
+      })
+
+    return
+  }
+  setTimeout(() => {
+    res.json(projects)
+  }, 2000)
+}
