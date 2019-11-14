e3de1271c8645165dd03250d65c2b759eadb6384
Sergio Xalambrí <hello@sergiodxa.com>
2019-11-14 15:37:31 -0500

    Make example uses preload and use other way for prefetching

---
+++ examples/prefetch-preload/pages/[user]/[repo].js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Head from "next/head"
+import Link from 'next/link'
+import fetch from '../../libs/fetch'
+
+import useSWR, { mutate } from 'swr'
+
+function prefetchParent() {
+  return fetch('/api/data')
+    .then(projects => mutate('/api/data', projects, false))
+}
+
+// if we are on the browser trigger a prefetch as soon as possible
+if (typeof window !== 'undefined') prefetchParent()
+
+export default () => {
+  const id = typeof window !== 'undefined' ? window.location.pathname.slice(1) : ''
+  const { data } = useSWR('/api/data?id=' + id, fetch)
+
+  React.useEffect(() => {
+    prefetchParent()
+  }, [])
+
+  function handleMouseEnter() {
+    prefetchParent()
+  }
+
+  return (
+    <>
+      <Head>
+        {/* This will tell the browser to preload the data for our page */}
+        {id && <link preload={`/api/data?id=${id}`} as="fetch" crossOrigin="anonymous" />}
+      </Head>
+      <div style={{ textAlign: 'center' }}>
+        <h1>{id}</h1>
+        {
+          data ? <div>
+            <p>forks: {data.forks_count}</p>
+            <p>stars: {data.stargazers_count}</p>
+            <p>watchers: {data.watchers}</p>
+          </div> : 'loading...'
+        }
+        <br />
+        <br />
+        <Link href="/"><a onMouseEnter={handleMouseEnter}>Back</a></Link>
+      </div>
+    </>
+  )
+}
---
+++ examples/prefetch-preload/pages/index.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import Head from "next/head";
+import Link from 'next/link'
+import fetch from '../libs/fetch'
+
+import useSWR, { mutate } from 'swr'
+
+function prefetchData() {
+  return fetch('/api/data')
+    .then(data => {
+      mutate('/api/data', data, false)
+      return data
+    })
+}
+
+function prefetchItem(project) {
+  return fetch(`https://api.github.com/repos/${project}`).then(data => {
+    mutate(`/api/data?id=${project}`, data, false)
+    return data
+  })
+}
+
+function prefetchWithProjects() {
+  return prefetchData()
+    .then(projects => Promise.all(projects.map(prefetchItem)))
+}
+
+// if we are on the browser trigger a prefetch as soon as possible
+if (typeof window !== 'undefined') prefetchWithProjects()
+
+export default () => {
+  const { data } = useSWR('/api/data', fetch)
+
+  // This effect will fetch all project after mounting
+  React.useEffect(() => {
+    if (!data) return
+    if (data.length === 0) return
+    data.forEach(prefetchItem)
+  }, [data]);
+
+  // With this handler you could prefetch the data for a specific
+  // project the moment the user move the mouse over the link
+  function handleMouseEnter(event) {
+    // In our case we could get the ID from the href so we use that
+    prefetchItem(event.target.getAttribute("href").slice(1))
+  }
+
+  return (
+    <>
+      <Head>
+        {/* This will tell the browser to preload the data for our page */}
+        <link preload="/api/data" as="fetch" crossOrigin="anonymous" />
+      </Head>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Trending Projects</h1>
+        <div>
+        {
+          data ? data.map(project => 
+            <p key={project}>
+              <Link href='/[user]/[repo]' as={`/${project}`}>
+                <a onMouseEnter={handleMouseEnter}>{project}</a>
+              </Link>
+            </p>
+          ) : 'loading...'
+        }
+        </div>
+      </div>
+    </>
+  )
+}
