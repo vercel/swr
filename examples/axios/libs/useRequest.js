5e6bde13f956f96b172484127fe452f750e753d9
Torleif Berger <torleif@outlook.com>
2019-11-21 20:05:07 +0100

    Added example for using axios with a request object

---
+++ examples/axios/libs/useRequest.js
@@ -0,0 +1,30 @@
+import useSWR from 'swr'
+import axios from 'axios'
+import { useMemo } from 'react'
+
+export default function useRequest(request, { initialData, ...config } = {}) {
+  const { data: response, error, isValidating, revalidate } = useSWR(
+    request && JSON.stringify(request),
+    () => axios(request || {}),
+    {
+      ...config,
+      initialData: initialData && {
+        status: 200,
+        statusText: 'InitialData',
+        headers: {},
+        data: initialData
+      }
+    }
+  )
+
+  return useMemo(
+    () => ({
+      data: response && response.data,
+      response,
+      error,
+      isValidating,
+      revalidate
+    }),
+    [response, error, isValidating, revalidate]
+  )
+}
