756fc70eb3dd4b5770cd416a7d9f0e33eed872da
patrick <patimpey@gmail.com>
2020-02-06 12:56:31 -0800

    null registering a length

--- src/use-swr-pages.tsx
+++ src/use-swr-pages.tsx
@@ -1,13 +1,5 @@
-import React, { useCallback, useMemo, useState, useRef } from 'react'
-
-import { cacheGet, cacheSet } from './config'
-import {
-  pagesResponseInterface,
-  responseInterface,
-  pageComponentType,
-  pageOffsetMapperType
-} from './types'
-
+import React, { useEffect, useCallback, useMemo, useState, useRef } from 'react';
+import { cacheGet, cacheSet } from './config';
 /*
 The idea
 
@@ -53,7 +45,8 @@ function App () {
     isLoadingMore,
     isReachingEnd,
     isEmpty,
-    loadMore
+    loadMore,
+    resetPages
   } = useSWRPages(
     'project-page', // key of this page
 
@@ -87,135 +80,103 @@ function App () {
   </div>
 }
 */
-
-const pageCacheMap = new Map()
-
-export function useSWRPages<OffsetType = any, Data = any, Error = any>(
-  pageKey: string,
-  pageFn: pageComponentType<OffsetType, Data, Error>,
-  SWRToOffset: pageOffsetMapperType<OffsetType, Data, Error>,
-  deps: any[] = []
-): pagesResponseInterface {
-  const pageCountKey = `_swr_page_count_` + pageKey
-  const pageOffsetKey = `_swr_page_offset_` + pageKey
-
-  const [pageCount, setPageCount] = useState<number>(
-    cacheGet(pageCountKey) || 1
-  )
-  const [pageOffsets, setPageOffsets] = useState<OffsetType[]>(
-    cacheGet(pageOffsetKey) || [null]
-  )
-  const [pageSWRs, setPageSWRs] = useState<responseInterface<Data, Error>[]>([])
-
-  const pageFnRef = useRef(pageFn)
-  const emptyPageRef = useRef(false)
-
+const pageCacheMap = new Map();
+export function useSWRPages(pageKey, pageFn, SWRToOffset, deps = []) {
+  const pageCountKey = useMemo(() => `_swr_page_count_` + pageKey, [pageKey]);
+  const pageOffsetKey = useMemo(() => `_swr_page_offset_` + pageKey, [pageKey]);
+  const [pageCount, setPageCount] = useState(cacheGet(pageCountKey) || 1);
+  const [pageOffsets, setPageOffsets] = useState(cacheGet(pageOffsetKey) || [null]);
+  const [pageSWRs, setPageSWRs] = useState([]);
+  const pageFnRef = useRef(pageFn);
+  const emptyPageRef = useRef(false);
   // Page component (wraps `pageFn`)
   // for performance reason we need to memorize it
   const Page = useCallback(props => {
     // render the page component
-    const dataList = pageFnRef.current(props)
-
+    const dataList = pageFnRef.current(props);
     // if dataList is [], we can assume this page is empty
     // TODO: this API is not stable
-    emptyPageRef.current = dataList && !dataList.length
-
-    return dataList
-  }, [])
-
+    emptyPageRef.current = dataList && !dataList.length;
+    return dataList;
+  }, []);
   const resetPages = useCallback(() => {
-    setPageSWRs([])
-
+    setPageSWRs([]);
     setPageCount(() => {
-      cacheSet(pageCountKey, 1)
-      return 1
-    })
-
+      cacheSet(pageCountKey, 1);
+      return 1;
+    });
     setPageOffsets(() => {
-      cacheSet(pageOffsetKey, [null])
-      return [null]
-    })
-  }, [])
-
+      cacheSet(pageOffsetKey, [null]);
+      return [null];
+    });
+  }, []);
   // Doesn't have a next page
-  const isReachingEnd = pageOffsets[pageCount] === null
-  const isLoadingMore = pageCount === pageOffsets.length
-  const isEmpty = isReachingEnd && pageCount === 1 && emptyPageRef.current
+  const isReachingEnd = pageOffsets[pageCount] === null;
+  const isLoadingMore = pageCount > pageOffsets.length;
+  const isEmpty = isReachingEnd && pageCount === 1 && emptyPageRef.current;
   const loadMore = useCallback(() => {
-    if (isLoadingMore || isReachingEnd) return
+    if (isLoadingMore || isReachingEnd)
+      return;
     setPageCount(c => {
-      cacheSet(pageCountKey, c + 1)
-      return c + 1
-    })
-  }, [isLoadingMore || isReachingEnd])
-  const _pageFn = useCallback(pageFn, deps)
-  pageFnRef.current = _pageFn
-
+      cacheSet(pageCountKey, c + 1);
+      return c + 1;
+    });
+  }, [isLoadingMore || isReachingEnd]);
+  const _pageFn = useCallback(pageFn, deps);
+  pageFnRef.current = _pageFn;
   const pages = useMemo(() => {
     const getWithSWR = id => swr => {
-      if (
-        !pageSWRs[id] ||
+      if (!pageSWRs[id] ||
         pageSWRs[id].data !== swr.data ||
         pageSWRs[id].error !== swr.error ||
-        pageSWRs[id].revalidate !== swr.revalidate
-      ) {
+        pageSWRs[id].revalidate !== swr.revalidate) {
         setPageSWRs(swrs => {
-          const _swrs = [...swrs]
+          const _swrs = [...swrs];
           _swrs[id] = {
             data: swr.data,
             error: swr.error,
             revalidate: swr.revalidate,
             isValidating: swr.isValidating
-          }
-          return _swrs
-        })
+          };
+          return _swrs;
+        });
         if (typeof swr.data !== 'undefined') {
           // set next page's offset
-          const newPageOffset = SWRToOffset(swr, id)
+          const newPageOffset = SWRToOffset(swr, id);
           if (pageOffsets[id + 1] !== newPageOffset) {
             setPageOffsets(arr => {
-              const _arr = [...arr]
-              _arr[id + 1] = newPageOffset
-              cacheSet(pageOffsetKey, _arr)
-              return _arr
-            })
+              const _arr = [...arr];
+              _arr[id + 1] = newPageOffset;
+              cacheSet(pageOffsetKey, _arr);
+              return _arr;
+            });
           }
         }
       }
-      return swr
-    }
-
+      return swr;
+    };
     // render each page
-    const p = []
+    const p = [];
     if (!pageCacheMap.has(pageKey)) {
-      pageCacheMap.set(pageKey, [])
+      pageCacheMap.set(pageKey, []);
     }
-    const pageCache = pageCacheMap.get(pageKey)
+    const pageCache = pageCacheMap.get(pageKey);
     for (let i = 0; i < pageCount; ++i) {
-      if (
-        !pageCache[i] ||
+      if (!pageCache[i] ||
         pageCache[i].offset !== pageOffsets[i] ||
-        pageCache[i].pageFn !== _pageFn
-      ) {
+        pageCache[i].pageFn !== _pageFn) {
         // when props change or at init
         // render the page and cache it
         pageCache[i] = {
-          component: (
-            <Page
-              key={`page-${pageOffsets[i]}-${i}`}
-              offset={pageOffsets[i]}
-              withSWR={getWithSWR(i)}
-            />
-          ),
+          component: (React.createElement(Page, { key: `page-${pageOffsets[i]}-${i}`, offset: pageOffsets[i], withSWR: getWithSWR(i) })),
           pageFn: _pageFn,
           offset: pageOffsets[i]
-        }
+        };
       }
-      p.push(pageCache[i].component)
+      p.push(pageCache[i].component);
     }
-    return p
-  }, [_pageFn, pageCount, pageSWRs, pageOffsets, pageKey])
-
+    return p;
+  }, [_pageFn, pageCount, pageSWRs, pageOffsets, pageKey]);
   return {
     pages,
     pageCount,
@@ -225,5 +186,5 @@ export function useSWRPages<OffsetType = any, Data = any, Error = any>(
     isEmpty,
     loadMore,
     resetPages
-  }
+  };
 }
