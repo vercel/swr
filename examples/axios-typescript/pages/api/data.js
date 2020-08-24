4566b2da97efb604354da34417163df731ea2335
Justin Michaliga <justin.michaliga@gmail.com>
2020-08-24 20:26:56 +0000

    fix: update for proper data handling

--- examples/axios-typescript/pages/api/data.js
+++ examples/axios-typescript/pages/api/data.js
@@ -11,7 +11,7 @@ export default (req, res) => {
   if (req.query.id) {
     // a slow endpoint for getting repo data
     axios(`https://api.github.com/repos/${req.query.id}`)
-      .then(resp => resp.json())
+      .then(resp => resp.data)
       .then(data => {
         setTimeout(() => {
           res.json(data)
--- examples/axios/pages/api/data.js
+++ examples/axios/pages/api/data.js
@@ -8,7 +8,12 @@ export default (req, res) => {
   if (req.query.id) {
     // a slow endpoint for getting repo data
     axios(`https://api.github.com/repos/${req.query.id}`)
-      .then(resp => resp.json())
+    .then(resp => resp.data)
+    .then(data => {
+      setTimeout(() => {
+        res.json(data)
+      }, 2000)
+    })
       .then(data => {
         setTimeout(() => {
           res.json(data)
