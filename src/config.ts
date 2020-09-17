7091de961b330697713204223b35044755dda839
Sergio Xalambrí <hello@sergiodxa.com>
2020-09-17 12:07:24 -0500

    Add a default fetcher using Fetch API (#367)

--- src/config.ts
+++ src/config.ts
@@ -66,7 +66,9 @@ const defaultConfig: ConfigInterface = {
   refreshWhenOffline: false,
   shouldRetryOnError: true,
   suspense: false,
-  compare: deepEqual
+  compare: deepEqual,
+
+  fetcher: url => fetch(url).then(res => res.json())
 }
 
 export { cache }
