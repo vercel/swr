2e88a3cacd3ad6d8cc67de77cfe360db0f47edf7
X <git@iamje.com>
2020-11-09 11:58:49 +0800

    refactor: improve Deno determining

--- src/use-swr.ts
+++ src/use-swr.ts
@@ -24,7 +24,7 @@ import {
   updaterInterface
 } from './types'
 
-const IS_SERVER = typeof window === 'undefined' || (typeof Deno !== 'undefined' && !!Deno?.version?.deno)
+const IS_SERVER = typeof window === 'undefined' || (typeof Deno !== 'undefined' && Deno !== null && !!Deno.version && !!Deno.version.deno)
 
 // polyfill for requestAnimationFrame
 const rAF = IS_SERVER
