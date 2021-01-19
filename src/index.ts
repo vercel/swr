98d0862f734cab596bd4058d38ad31a9d11b5856
Shu Ding <g@shud.in>
2021-01-19 23:59:10 +0800

    add __esModule mark

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './use-swr'
-export { default } from './use-swr'
 export {
   useSWRInfinite,
   SWRInfiniteConfigInterface,
@@ -14,3 +13,10 @@ export {
   responseInterface,
   CacheInterface
 } from './types'
+
+// A work around for the `__esModule` mark as microbundle
+// doesn't support mixed exports currently.
+// https://github.com/developit/microbundle/issues/306#issuecomment-459103476
+import * as exports from './use-swr'
+Object.defineProperty(exports, '__esModule', { value: true })
+export default exports
