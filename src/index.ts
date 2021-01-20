a360ebc4b8266e7001a7ed3c32f9998a85ce6caa
huozhi <inbox@huozhi.im>
2021-01-20 23:39:58 +0800

    revert last workaround

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './use-swr'
+import { default as useSWR } from './use-swr'
 export {
   useSWRInfinite,
   SWRInfiniteConfigInterface,
@@ -14,9 +14,4 @@ export {
   CacheInterface
 } from './types'
 
-// A work around for the `__esModule` mark as microbundle
-// doesn't support mixed exports currently.
-// https://github.com/developit/microbundle/issues/306#issuecomment-459103476
-import * as exports from './use-swr'
-Object.defineProperty(exports, '__esModule', { value: true })
-export default exports
+export default useSWR
