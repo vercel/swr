8d1546202edd552b47c509cbb9944674a49aab91
Toru Kobayashi <koba0004@gmail.com>
2021-02-03 00:57:03 +0900

    types: makes types fn and config and getKey more strictly

--- src/swr-config-context.ts
+++ src/swr-config-context.ts
@@ -2,7 +2,7 @@ import { createContext } from 'react'
 
 import { ConfigInterface } from './types'
 
-const SWRConfigContext = createContext<ConfigInterface>({})
+const SWRConfigContext = createContext<Partial<ConfigInterface>>({})
 SWRConfigContext.displayName = 'SWRConfigContext'
 
 export default SWRConfigContext
