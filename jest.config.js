89d0f20d17e92b79786a0d6c6073d0e7b7bafa57
Yixuan Xu <yixuanxu94@outlook.com>
2021-03-05 20:57:44 +0800

    add a new tsconfig.test.json for ts-jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testRegex: '/test/.*\\.test\\.tsx$',
   modulePathIgnorePatterns: ['<rootDir>/examples/'],
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json'
+    }
+  }
 }
