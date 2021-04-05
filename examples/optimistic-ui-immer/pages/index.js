e73d9fe4cd58d5305fd4ce0e54e70fde31c7d720
Roman Mkrtchian <mkrtchia@gmail.com>
2021-04-05 19:10:35 +0200

    docs: optimistic UI examples comments more clearer (#1102)

--- examples/optimistic-ui-immer/pages/index.js
+++ examples/optimistic-ui-immer/pages/index.js
@@ -10,15 +10,19 @@ export default () => {
 
   async function handleSubmit(event) {
     event.preventDefault()
-    // call mutate to optimistically update the UI
-    // we use Immer produce to allow us to perform and immutable change
-    // while coding it as a normal mutation of the same object
+    // Call mutate to optimistically update the UI.
+    // We use Immer produce to allow us to perform an immutable change
+    // while coding it as a normal mutation of the same object.
     mutate("/api/data", produce(draftData => {
       draftData.push(text)
     }), false)
-    // then we send the request to the API and let mutate
-    // update the data with the API response
-    // if this fail it will rollback the optimistic update
+    // Then we send the request to the API and let mutate
+    // update the data with the API response.
+    // Our action may fail in the API function, and the response differ
+    // from what was optimistically updated, in that case the UI will be
+    // changed to match the API response.
+    // The fetch could also fail, in that case the UI will
+    // be in an incorrect state until the next successful fetch.
     mutate('/api/data', await fetch('/api/data', {
       method: 'POST',
       body: JSON.stringify({ text })
