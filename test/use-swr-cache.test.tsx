e8c21fa13ced85642f87033e0a385d06180edc4b
Shu Ding <g@shud.in>
2021-05-02 15:05:16 +0800

    add isolated cache test

--- test/use-swr-cache.test.tsx
+++ test/use-swr-cache.test.tsx
@@ -107,4 +107,34 @@ describe('useSWR - cache', () => {
     )
     screen.getByText('1:2')
   })
+
+  it('should support isolated cache', async () => {
+    const key = createKey()
+    const customCache1 = new Map([[key, '1']])
+    const { cache: cache1 } = createCache(customCache1)
+    const customCache2 = new Map([[key, '2']])
+    const { cache: cache2 } = createCache(customCache2)
+
+    // Nested components with the same cache key can get different values.
+    function Foo() {
+      const { data } = useSWR(key, null)
+      return <>{data}</>
+    }
+    function Page() {
+      return (
+        <div>
+          <SWRConfig value={{ cache: cache1 }}>
+            <Foo />
+          </SWRConfig>
+          :
+          <SWRConfig value={{ cache: cache2 }}>
+            <Foo />
+          </SWRConfig>
+        </div>
+      )
+    }
+
+    render(<Page />)
+    screen.getByText('1:2')
+  })
 })
