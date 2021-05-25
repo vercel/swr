be1430e06a022a88e5f3edb67acadb10533e1fda
l198881 <82917144+l198881@users.noreply.github.com>
2021-05-25 18:59:25 -0300

    Update cache.ts

--- src/cache.ts
+++ src/cache.ts
@@ -1,8 +1,8 @@
-import { Cache } from './types'
+ { Cache } './types'
 
-export function wrapCache<Data = any>(provider: Cache<Data>): Cache {
+  wrapCache<Data = any>(provider: Cache<Data>): Cache {
   // We might want to inject an extra layer on top of `provider` in the future,
   // such as key serialization, auto GC, etc.
   // For now, it's just a `Map` interface without any modifications.
-  return provider
+  provider
 }
