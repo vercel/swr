bbf91219da82ec9a1579cf4ab48fd7b9f82884b0
Shu Ding <g@shud.in>
2021-06-18 19:17:46 +0200

    fix failed tests

--- src/libs/merge-config.ts
+++ src/libs/merge-config.ts
@@ -1,13 +1,13 @@
 import { Configuration } from '../types'
 
 export default function mergeConfig(
-  a: Partial<Configuration>,
-  b: Partial<Configuration>
+  a: Partial<Configuration> | null,
+  b: Partial<Configuration> | null
 ) {
-  const v = { ...a, ...b }
+  const v: Partial<Configuration> = { ...a, ...b }
 
-  const m1 = a.middlewares
-  const m2 = b.middlewares
+  const m1 = a && a.middlewares
+  const m2 = b && b.middlewares
   if (m1 && m2) {
     v.middlewares = m1.concat(m2)
   }
