99acf44a4fea97cecc79390902aea2f046348015
Shu Ding <g@shud.in>
2021-06-29 22:43:42 +0200

    fix test

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   testRegex: '/test/.*\\.test\\.tsx$',
   modulePathIgnorePatterns: ['<rootDir>/examples/'],
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  moduleNameMapper: {
+    swr: '<rootDir>/dist/index.js'
+  },
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json'
