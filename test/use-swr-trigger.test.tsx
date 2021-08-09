fab39827b964eb51262b0ba86070ee62f43e9140
Shu Ding <g@shud.in>
2021-08-10 00:05:16 +0200

    add more tests

--- test/use-swr-trigger.test.tsx
+++ test/use-swr-trigger.test.tsx
@@ -24,4 +24,56 @@ describe('useSWR - trigger', () => {
 
     screen.getByText('data')
   })
+
+  it('should trigger request with correct args', async () => {
+    const key = createKey()
+    const fetcher = jest.fn(() => 'data')
+
+    function Page() {
+      const { data, trigger } = useSWRTrigger(key, fetcher)
+      return (
+        <button onClick={() => trigger('arg1', 'arg2')}>
+          {data || 'pending'}
+        </button>
+      )
+    }
+
+    render(<Page />)
+
+    // mount
+    await screen.findByText('pending')
+
+    fireEvent.click(screen.getByText('pending'))
+    await waitForNextTick()
+
+    screen.getByText('data')
+
+    expect(fetcher).toHaveBeenCalled()
+    expect(fetcher.mock.calls.length).toBe(1)
+    expect(fetcher.mock.calls[0]).toEqual([key, 'arg1', 'arg2'])
+  })
+
+  it('should call `onSuccess` event', async () => {
+    const key = createKey()
+    const onSuccess = jest.fn()
+
+    function Page() {
+      const { data, trigger } = useSWRTrigger(key, () => 'data', {
+        onSuccess
+      })
+      return <button onClick={() => trigger()}>{data || 'pending'}</button>
+    }
+
+    render(<Page />)
+
+    // mount
+    await screen.findByText('pending')
+
+    fireEvent.click(screen.getByText('pending'))
+    await waitForNextTick()
+
+    screen.getByText('data')
+
+    expect(onSuccess).toHaveBeenCalled()
+  })
 })
