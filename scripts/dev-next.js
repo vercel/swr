4c4164a11f12860a315427080c62e28632c0b461
Jiachi Liu <inbox@huozhi.im>
2021-08-18 20:02:30 +0800

    fix: alias swr in next client bundle on dev mode

--- scripts/dev-next.js
+++ scripts/dev-next.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const { existsSync } = require('fs')
+const fs = require('fs')
 const { resolve } = require('path')
-const { execSync } = require('child_process')
+const childProcess = require('child_process')
 
 const args = process.argv
 const target = args[2]
@@ -21,13 +21,37 @@ const examplesDir = resolve(__dirname, '../examples')
 const exampleDir = resolve(examplesDir, target)
 
 const nextBin = resolve(exampleDir, 'node_modules/.bin/next')
-if (!existsSync(nextBin)) {
+if (!fs.existsSync(nextBin)) {
   error(`Please run "yarn install" inside ${target} example directory\n`)
 }
 
 const requireHookPath = resolve(__dirname, 'require-hook.js')
-const devCmd = `cd ${exampleDir} && node -r ${requireHookPath} ${nextBin} ${nextCmd}`
+const nextConfigPath = resolve(exampleDir, 'next.config.js')
+const devCmd = `node -r ${requireHookPath} ${nextBin} ${nextCmd}`.split(' ')
 
-console.log(`Running ${devCmd}`)
+if (!fs.existsSync(nextConfigPath)) {
+  fs.symlinkSync(resolve(__dirname, 'next-config.js'), nextConfigPath, 'file')
+}
 
-execSync(devCmd, { stdio: 'inherit' })
+const sub = childProcess.spawn(devCmd.shift(), devCmd, { cwd: exampleDir })
+
+const logStdout = data => process.stdout.write(data.toString())
+const logStderr = data => process.stderr.write(data.toString())
+
+sub.stdout.on('data', logStdout)
+sub.stderr.on('data', logStderr)
+sub.stderr.on('close', () => {
+  sub.stdout.off('data', logStdout)
+  sub.stderr.off('data', logStderr)
+})
+
+const teardown = () => {
+  if (sub.killed) sub.kill('SIGINT')
+  if (fs.existsSync(nextConfigPath)) fs.unlinkSync(nextConfigPath)
+}
+
+process.on('exit', teardown)
+process.on('SIGINT', teardown)
+
+// main
+console.log(`Running ${devCmd.join(' ')}`)
