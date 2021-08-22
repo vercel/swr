6ac8c38cca98f00a8a1c28917ad7ae2eb2f0092d
Shu Ding <g@shud.in>
2021-08-23 01:22:37 +0200

    refactor

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -1,7 +1,7 @@
 import { useContext, useState } from 'react'
 import { defaultConfig } from './config'
 import { wrapCache } from './cache'
-import { isFunction, UNDEFINED } from './helper'
+import { isFunction } from './helper'
 import { SWRConfigContext } from './config-context'
 import { SWRGlobalState, GlobalState } from './global-state'
 
@@ -17,21 +17,17 @@ export const useSWRProvider = <Data = any>(
 
   // We use a state to keep the cache instance and make sure it's only
   // initialized once.
-  const [newProvider] = useState(() => {
-    if (isFunction(initialize))
+  return useState(() => {
+    if (isFunction(initialize)) {
       return wrapCache<Data>(initialize(cache), options)
-    else if (initialize) {
+    } else if (initialize) {
       return wrapCache<Data>(cache, initialize)
     }
-    return UNDEFINED
-  })
 
-  // Return the new provider if it's created, otherwise return the current
-  // provider.
-  return (
-    newProvider || {
+    // If no arg is present, return the current provider.
+    return {
       cache,
       mutate: (SWRGlobalState.get(cache) as GlobalState)[6]
     }
-  )
+  })[0]
 }
