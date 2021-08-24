0716abf3b9a16f0224220ef7bb554b793e7f507a
fsubal <fsubal@users.noreply.github.com>
2021-08-24 21:24:47 +0900

    use void 0 for undefined
    ({})[0] can still be replaced to something else

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,6 +1,5 @@
 // `undefined` can possibly be replaced by something else.
-export const UNDEFINED: undefined = ({} as any)[0]
-export const isUndefined = (v: any): v is undefined => v === UNDEFINED
+export const isUndefined = (v: any): v is undefined => v === void 0
 export const isFunction = (v: any): v is Function => typeof v === 'function'
 export const noop = () => {}
 export const mergeObjects = (a: any, b: any) => Object.assign({}, a, b)
