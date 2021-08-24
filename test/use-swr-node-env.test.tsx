8f7f54640b20a0835ae847babb4a0649c74a3185
Jiachi Liu <inbox@huozhi.im>
2021-08-25 01:47:10 +0800

    add is server flag checking

--- test/use-swr-node-env.test.tsx
+++ test/use-swr-node-env.test.tsx
@@ -7,8 +7,13 @@ import { renderToString } from 'react-dom/server'
 import useSWR from '../src'
 import useSWRImmutable from '../immutable'
 import { createKey } from './utils'
+import { IS_SERVER } from '../src/utils/env'
 
 describe('useSWR', () => {
+  it('env IS_SERVER is true in node env', () => {
+    expect(IS_SERVER).toBe(true)
+  })
+
   it('should render fallback if provided on server side', async () => {
     const key = createKey()
     const useData = () => useSWR(key, k => k, { fallbackData: 'fallback' })
