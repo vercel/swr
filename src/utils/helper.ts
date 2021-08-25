48ece8126c1b46cd0a73330860f80c58be642184
Jiachi Liu <inbox@huozhi.im>
2021-08-25 21:13:29 +0800

    enhance: safe undefined variable generation

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,5 +1,5 @@
 // `undefined` can possibly be replaced by something else.
-export const UNDEFINED: undefined = ({} as any)[0]
+export const UNDEFINED: undefined = (NaN as any)[0]
 export const isUndefined = (v: any): v is undefined => v === UNDEFINED
 export const isFunction = (v: any): v is Function => typeof v === 'function'
 export const noop = () => {}
