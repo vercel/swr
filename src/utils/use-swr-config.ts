7eb78f03d241427c02429e21ab6dcae430343a31
Shuman Lim <18899758+origin-master@users.noreply.github.com>
2021-08-30 17:32:24 +0900

    fix: `useSWRConfig` is exported as `() => any` type

--- src/utils/use-swr-config.ts
+++ src/utils/use-swr-config.ts
@@ -2,7 +2,8 @@ import { useContext } from 'react'
 import { defaultConfig } from './config'
 import { SWRConfigContext } from './config-context'
 import { mergeObjects } from './helper'
+import { FullConfiguration } from '../types'
 
-export const useSWRConfig = () => {
+export const useSWRConfig = (): FullConfiguration => {
   return mergeObjects(defaultConfig, useContext(SWRConfigContext))
 }
