8f779abb484417f5e3bc98042491671e9d5bd5cf
Jiachi Liu <inbox@huozhi.im>
2021-09-28 12:38:44 +0800

    example: add ref checking condition

--- examples/infinite-scroll/hooks/useOnScreen.js
+++ examples/infinite-scroll/hooks/useOnScreen.js
@@ -4,6 +4,8 @@ export default function useOnScreen(ref) {
   const [isIntersecting, setIntersecting] = useState(false)
 
   useEffect(() => {
+    if (!ref.current) return
+
     const observer = new IntersectionObserver(([entry]) =>
       setIntersecting(entry.isIntersecting)
     )
