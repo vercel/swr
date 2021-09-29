2f6051a7e01c67b41ad594b5b2ea64b0609dfd6e
Shu Ding <g@shud.in>
2021-09-29 14:11:48 +0200

    output code coverage

--- jest.config.js
+++ jest.config.js
@@ -14,5 +14,8 @@ module.exports = {
       tsconfig: 'test/tsconfig.json',
       diagnostics: process.env.CI
     }
-  }
+  },
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '/test/'],
+  coverageProvider: 'v8',
+  coverageReporters: ['text']
 }
