5caedaa42ea5a30b4471f98e124cad09d115c9ee
Shu Ding <g@shud.in>
2021-10-02 13:28:29 +0200

    chore: Code refactoring (#1521)
    * adjust condition
    
    * use .catch

--- src/utils/merge-config.ts
+++ src/utils/merge-config.ts
@@ -8,15 +8,16 @@ export const mergeConfigs = (
   // Need to create a new object to avoid mutating the original here.
   const v: Partial<FullConfiguration> = mergeObjects(a, b)
 
-  if (!b) return v
-
-  const { use: u1, fallback: f1 } = a
-  const { use: u2, fallback: f2 } = b
-  if (u1 && u2) {
-    v.use = u1.concat(u2)
-  }
-  if (f1 && f2) {
-    v.fallback = mergeObjects(f1, f2)
+  // If two configs are provided, merge their `use` and `fallback` options.
+  if (b) {
+    const { use: u1, fallback: f1 } = a
+    const { use: u2, fallback: f2 } = b
+    if (u1 && u2) {
+      v.use = u1.concat(u2)
+    }
+    if (f1 && f2) {
+      v.fallback = mergeObjects(f1, f2)
+    }
   }
 
   return v
