8c6b5e87ca8b36b8eb12ae51315a22d5065d935b
Jiachi Liu <inbox@huozhi.im>
2022-01-11 18:25:16 +0100

    revert withArgs export

--- src/utils/resolve-args.ts
+++ src/utils/resolve-args.ts
@@ -4,7 +4,7 @@ import { useSWRConfig } from './use-swr-config'
 
 // It's tricky to pass generic types as parameters, so we just directly override
 // the types here.
-export function withArgs<SWRType>(hook: any) {
+export const withArgs = <SWRType>(hook: any) => {
   return function useSWRArgs(...args: any) {
     // Get the default and inherited configuration.
     const fallbackConfig = useSWRConfig()
