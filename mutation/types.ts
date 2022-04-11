94f3579353cb7a3cb7177d14d7e57e50aaf659eb
Shu Ding <g@shud.in>
2022-04-11 18:31:01 +0200

    update typing

--- mutation/types.ts
+++ mutation/types.ts
@@ -1,79 +1,56 @@
-import { SWRResponse, SWRConfiguration, Key, MutatorOptions } from 'swr'
+import { SWRResponse, Key, MutatorOptions } from 'swr'
 
-type Async<Data> = Data | Promise<Data>
+type FetcherResponse<Data> = Data | Promise<Data>
+
+type FetcherOptions<ExtraArg = unknown> = Readonly<{
+  arg: ExtraArg
+}>
 
 export type MutationFetcher<
   Data = unknown,
-  SWRKey extends Key = Key,
-  ExtraArg = any
-> =
-  /**
-   * () => [{ foo: string }, { bar: number }] | null
-   * () => ( [{ foo: string }, { bar: number } ] as const | null )
-   */
-  SWRKey extends () => readonly [...infer Args] | null
-    ? (...args: [...Args, ExtraArg]) => Async<Data>
-    : /**
-     * [{ foo: string }, { bar: number } ] | null
-     * [{ foo: string }, { bar: number } ] as const | null
-     */
-    SWRKey extends readonly [...infer Args]
-    ? (...args: [...Args, ExtraArg]) => Async<Data>
-    : /**
-     * () => string | null
-     * () => Record<any, any> | null
-     */
-    SWRKey extends () => infer Arg | null
-    ? (...args: [Arg, ExtraArg]) => Async<Data>
-    : /**
-     *  string | null | Record<any,any>
-     */
-    SWRKey extends null
-    ? never
-    : SWRKey extends infer Arg
-    ? (...args: [Arg, ExtraArg]) => Async<Data>
-    : never
+  ExtraArg = unknown,
+  SWRKey extends Key = Key
+> = SWRKey extends () => infer Arg | null | undefined | false
+  ? (key: Arg, options: FetcherOptions<ExtraArg>) => FetcherResponse<Data>
+  : SWRKey extends null | undefined | false
+  ? never
+  : SWRKey extends infer Arg
+  ? (key: Arg, options: FetcherOptions<ExtraArg>) => FetcherResponse<Data>
+  : never
 
 export type SWRMutationConfiguration<
   Data,
   Error,
-  SWRMutationKey extends Key = Key,
-  ExtraArg = any
-> = Pick<
-  SWRConfiguration<
-    Data,
-    Error,
-    MutationFetcher<Data, SWRMutationKey, ExtraArg>
-  >,
-  'fetcher'
-> &
-  MutatorOptions<Data> & {
-    onSuccess?: (
-      data: Data,
-      key: string,
-      config: Readonly<
-        SWRMutationConfiguration<Data, Error, SWRMutationKey, ExtraArg>
-      >
-    ) => void
-    onError?: (
-      err: Error,
-      key: string,
-      config: Readonly<
-        SWRMutationConfiguration<Data, Error, SWRMutationKey, ExtraArg>
-      >
-    ) => void
-  }
+  ExtraArg = any,
+  SWRMutationKey extends Key = Key
+> = MutatorOptions<Data> & {
+  fetcher?: MutationFetcher<Data, ExtraArg, SWRMutationKey>
+  onSuccess?: (
+    data: Data,
+    key: string,
+    config: Readonly<
+      SWRMutationConfiguration<Data, Error, SWRMutationKey, ExtraArg>
+    >
+  ) => void
+  onError?: (
+    err: Error,
+    key: string,
+    config: Readonly<
+      SWRMutationConfiguration<Data, Error, SWRMutationKey, ExtraArg>
+    >
+  ) => void
+}
 
 export interface SWRMutationResponse<
   Data = any,
   Error = any,
-  SWRMutationKey extends Key = Key,
-  ExtraArg = any
-> extends Omit<SWRResponse<Data, Error>, 'isValidating'> {
+  ExtraArg = any,
+  SWRMutationKey extends Key = Key
+> extends Pick<SWRResponse<Data, Error>, 'data' | 'error'> {
   isMutating: boolean
   trigger: (
     extraArgument?: ExtraArg,
-    options?: SWRMutationConfiguration<Data, Error, SWRMutationKey, ExtraArg>
+    options?: SWRMutationConfiguration<Data, Error, ExtraArg, SWRMutationKey>
   ) => Promise<Data | undefined>
   reset: () => void
 }
@@ -85,10 +62,10 @@ export type SWRMutationHook = <
   ExtraArg = any
 >(
   ...args:
-    | readonly [SWRMutationKey, MutationFetcher<Data, SWRMutationKey, ExtraArg>]
+    | readonly [SWRMutationKey, MutationFetcher<Data, ExtraArg, SWRMutationKey>]
     | readonly [
         SWRMutationKey,
-        MutationFetcher<Data, SWRMutationKey, ExtraArg>,
-        SWRMutationConfiguration<Data, Error, SWRMutationKey, ExtraArg>
+        MutationFetcher<Data, ExtraArg, SWRMutationKey>,
+        SWRMutationConfiguration<Data, Error, ExtraArg, SWRMutationKey>
       ]
-) => SWRMutationResponse<Data, Error, SWRMutationKey, ExtraArg>
+) => SWRMutationResponse<Data, Error, ExtraArg, SWRMutationKey>
