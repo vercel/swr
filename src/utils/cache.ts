7839c186feb9761dc44b6966177e305ca9a86caf
Yixuan Xu <yixuanxu94@outlook.com>
2022-05-11 01:33:45 +0800

    fix: custom cache init

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -26,7 +26,7 @@ export const initCache = <Data = any>(
   provider: Cache<Data>,
   options?: Partial<ProviderConfiguration>
 ):
-  | [Cache<Data>, ScopedMutator<Data>, () => void]
+  | [Cache<Data>, ScopedMutator<Data>, () => void, () => void]
   | [Cache<Data>, ScopedMutator<Data>]
   | undefined => {
   // The global state for a specific provider will be used to deduplicate
@@ -45,6 +45,7 @@ export const initCache = <Data = any>(
       UNDEFINED,
       provider
     ) as ScopedMutator<Data>
+    let unmounted = false
     let unmount = noop
 
     const subscriptions: Record<string, ((current: any, prev: any) => void)[]> =
@@ -62,8 +63,8 @@ export const initCache = <Data = any>(
       }
     }
     const setter = (key: string, value: any, prev: any) => {
+      if (unmounted) return
       provider.set(key, value)
-
       const subs = subscriptions[key]
       if (subs) {
         for (let i = subs.length; i--; ) {
@@ -72,60 +73,66 @@ export const initCache = <Data = any>(
       }
     }
 
-    // Update the state if it's new, or the provider has been extended.
-    SWRGlobalState.set(provider, [
-      EVENT_REVALIDATORS,
-      {},
-      {},
-      mutate,
-      setter,
-      subscribe
-    ])
+    const initProvider = () => {
+      if (!SWRGlobalState.has(provider)) {
+        // Update the state if it's new, or the provider has been extended.
+        SWRGlobalState.set(provider, [
+          EVENT_REVALIDATORS,
+          {},
+          {},
+          mutate,
+          setter,
+          subscribe
+        ])
+        unmounted = false
+        if (!IS_SERVER) {
+          // When listening to the native events for auto revalidations,
+          // we intentionally put a delay (setTimeout) here to make sure they are
+          // fired after immediate JavaScript executions, which can possibly be
+          // React's state updates.
+          // This avoids some unnecessary revalidations such as
+          // https://github.com/vercel/swr/issues/1680.
+          const releaseFocus = opts.initFocus(
+            setTimeout.bind(
+              UNDEFINED,
+              revalidateAllKeys.bind(
+                UNDEFINED,
+                EVENT_REVALIDATORS,
+                revalidateEvents.FOCUS_EVENT
+              )
+            )
+          )
+          const releaseReconnect = opts.initReconnect(
+            setTimeout.bind(
+              UNDEFINED,
+              revalidateAllKeys.bind(
+                UNDEFINED,
+                EVENT_REVALIDATORS,
+                revalidateEvents.RECONNECT_EVENT
+              )
+            )
+          )
+          unmount = () => {
+            releaseFocus && releaseFocus()
+            releaseReconnect && releaseReconnect()
+            unmounted = true
+            // When un-mounting, we need to remove the cache provider from the state
+            // storage too because it's a side-effect. Otherwise when re-mounting we
+            // will not re-register those event listeners.
+            SWRGlobalState.delete(provider)
+          }
+        }
+      }
+    }
+    initProvider()
 
     // This is a new provider, we need to initialize it and setup DOM events
     // listeners for `focus` and `reconnect` actions.
-    if (!IS_SERVER) {
-      // When listening to the native events for auto revalidations,
-      // we intentionally put a delay (setTimeout) here to make sure they are
-      // fired after immediate JavaScript executions, which can possibly be
-      // React's state updates.
-      // This avoids some unnecessary revalidations such as
-      // https://github.com/vercel/swr/issues/1680.
-      const releaseFocus = opts.initFocus(
-        setTimeout.bind(
-          UNDEFINED,
-          revalidateAllKeys.bind(
-            UNDEFINED,
-            EVENT_REVALIDATORS,
-            revalidateEvents.FOCUS_EVENT
-          )
-        )
-      )
-      const releaseReconnect = opts.initReconnect(
-        setTimeout.bind(
-          UNDEFINED,
-          revalidateAllKeys.bind(
-            UNDEFINED,
-            EVENT_REVALIDATORS,
-            revalidateEvents.RECONNECT_EVENT
-          )
-        )
-      )
-      unmount = () => {
-        releaseFocus && releaseFocus()
-        releaseReconnect && releaseReconnect()
-
-        // When un-mounting, we need to remove the cache provider from the state
-        // storage too because it's a side-effect. Otherwise when re-mounting we
-        // will not re-register those event listeners.
-        SWRGlobalState.delete(provider)
-      }
-    }
 
     // We might want to inject an extra layer on top of `provider` in the future,
     // such as key serialization, auto GC, etc.
     // For now, it's just a `Map` interface without any modifications.
-    return [provider, mutate, unmount]
+    return [provider, mutate, initProvider, unmount]
   }
 
   return [provider, (SWRGlobalState.get(provider) as GlobalState)[3]]
