3cd796b724e790569a96ab62c99f49e2d2e72895
Yixuan Xu <yixuanxu94@outlook.com>
2022-05-14 17:17:12 +0800

    types: fix type regressions (#1966)

--- core/index.ts
+++ core/index.ts
@@ -18,11 +18,11 @@ export type {
   SWRHook,
   SWRResponse,
   Cache,
-  CacheValue,
   BareFetcher,
   Fetcher,
   MutatorCallback,
   MutatorOptions,
   Middleware,
-  Arguments
+  Arguments,
+  State
 } from 'swr/_internal'
