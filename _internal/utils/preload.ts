3501ca90e43a1b663e6a3628d37c593d0916ca35
Toru Kobayashi <koba0004@gmail.com>
2022-06-16 10:10:19 +0900

    refactor: variable names

--- _internal/utils/preload.ts
+++ _internal/utils/preload.ts
@@ -1,29 +1,30 @@
 import { Middleware, Key, BareFetcher } from '../types'
 import { serialize } from './serialize'
 
-const preloadMap = new Map<string, Promise<any>>()
+const REQUEST = new Map<string, Promise<any>>()
 
-export const preload = (key: Key, fetcher: BareFetcher<any>) => {
-  const promise = fetcher(key)
-  const keyString = serialize(key)[0]
-  preloadMap.set(keyString, promise)
-  return promise
+export const preload = (key_: Key, fetcher: BareFetcher<any>) => {
+  const req = fetcher(key_)
+  const key = serialize(key_)[0]
+  REQUEST.set(key, req)
+  return req
 }
 
-export const middleware: Middleware = useSWRNext => (key, fetcher_, config) => {
-  const fetcher =
-    fetcher_ == null
-      ? null
-      : // fetcher might be a sync function, so this should not be an async function
-        (...args: any[]) => {
-          const keyString = serialize(key)[0]
-          const promise = preloadMap.get(keyString)
-          if (promise) {
-            preloadMap.delete(keyString)
-            return promise
+export const middleware: Middleware =
+  useSWRNext => (key_, fetcher_, config) => {
+    const fetcher =
+      fetcher_ == null
+        ? null
+        : // fetcher might be a sync function, so this should not be an async function
+          (...args: any[]) => {
+            const key = serialize(key_)[0]
+            const req = REQUEST.get(key)
+            if (req) {
+              REQUEST.delete(key)
+              return req
+            }
+            return fetcher_(...args)
           }
-          return fetcher_(...args)
-        }
 
-  return useSWRNext(key, fetcher, config)
-}
+    return useSWRNext(key_, fetcher, config)
+  }
