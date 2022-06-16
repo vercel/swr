7f98cc1a20168c2a00c34eb937dd8a0fe8d9b264
Toru Kobayashi <koba0004@gmail.com>
2022-06-16 18:02:49 +0900

    refactor: ignore falsy fetcher

--- _internal/utils/preload.ts
+++ _internal/utils/preload.ts
@@ -12,19 +12,17 @@ export const preload = <Data = any>(key_: Key, fetcher: BareFetcher<Data>) => {
 
 export const middleware: Middleware =
   useSWRNext => (key_, fetcher_, config) => {
+    // fetcher might be a sync function, so this should not be an async function
     const fetcher =
-      fetcher_ == null
-        ? null
-        : // fetcher might be a sync function, so this should not be an async function
-          (...args: any[]) => {
-            const key = serialize(key_)[0]
-            const req = REQUEST.get(key)
-            if (req) {
-              REQUEST.delete(key)
-              return req
-            }
-            return fetcher_(...args)
-          }
-
+      fetcher_ &&
+      ((...args: any[]) => {
+        const key = serialize(key_)[0]
+        const req = REQUEST.get(key)
+        if (req) {
+          REQUEST.delete(key)
+          return req
+        }
+        return fetcher_(...args)
+      })
     return useSWRNext(key_, fetcher, config)
   }
