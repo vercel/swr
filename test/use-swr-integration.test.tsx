993e0357c46bf554ed67bf4949af959eead20691
Toru Kobayashi <koba0004@gmail.com>
2022-09-01 03:31:40 +0900

    test: add tests for dependency collection (#2140)

--- test/use-swr-integration.test.tsx
+++ test/use-swr-integration.test.tsx
@@ -470,4 +470,103 @@ describe('useSWR', () => {
     await act(() => sleep(150))
     expect(count).toBe(2)
   })
+
+  // Test for https://swr.vercel.app/docs/advanced/performance#dependency-collection
+  it('should render four times in the worst case', async () => {
+    let isFirstFetch = true
+    const fetcher = async () => {
+      if (isFirstFetch) {
+        isFirstFetch = false
+        throw new Error('error')
+      }
+      return 'value'
+    }
+    const key = createKey()
+
+    const logs = []
+
+    function Page() {
+      const { data, error, isLoading, isValidating } = useSWR(key, fetcher, {
+        errorRetryInterval: 10
+      })
+      logs.push({
+        data,
+        error,
+        isLoading,
+        isValidating
+      })
+      if (isLoading) return <p>loading</p>
+      return <p>data:{data}</p>
+    }
+
+    renderWithConfig(<Page />)
+    await screen.findByText('data:value')
+
+    expect(logs).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "data": undefined,
+          "error": undefined,
+          "isLoading": true,
+          "isValidating": true,
+        },
+        Object {
+          "data": undefined,
+          "error": [Error: error],
+          "isLoading": false,
+          "isValidating": false,
+        },
+        Object {
+          "data": undefined,
+          "error": [Error: error],
+          "isLoading": true,
+          "isValidating": true,
+        },
+        Object {
+          "data": "value",
+          "error": undefined,
+          "isLoading": false,
+          "isValidating": false,
+        },
+      ]
+    `)
+  })
+
+  // Test for https://swr.vercel.app/docs/advanced/performance#dependency-collection
+  it('should render only two times in the best case', async () => {
+    let isFirstFetch = true
+    const fetcher = async () => {
+      if (isFirstFetch) {
+        isFirstFetch = false
+        throw new Error('error')
+      }
+      return 'value'
+    }
+    const key = createKey()
+
+    const logs = []
+
+    function Page() {
+      const { data } = useSWR(key, fetcher, {
+        errorRetryInterval: 10
+      })
+      logs.push({ data })
+      if (!data) return <p>loading</p>
+      return <p>data:{data}</p>
+    }
+
+    renderWithConfig(<Page />)
+    await screen.findByText('data:value')
+
+    expect(logs).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "data": undefined,
+        },
+        Object {
+          "data": "value",
+        },
+      ]
+    `)
+  })
 })
