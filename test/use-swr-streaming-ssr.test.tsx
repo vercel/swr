bea16ac3b16cce5d9be9170dd9d6b80c1dd1abc1
Shu Ding <g@shud.in>
2023-01-27 22:53:44 +0100

    mark as failing

--- test/use-swr-streaming-ssr.test.tsx
+++ test/use-swr-streaming-ssr.test.tsx
@@ -32,50 +32,53 @@ describe('useSWR - streaming', () => {
     ensureAndUnmock()
   })
 
-  // NOTE: this test is skipped because it's not possible to test this behavior
+  // NOTE: this test is failing because it's not possible to test this behavior
   // in JSDOM. We need to test this in a real browser.
-  it.skip('should match the ssr result when streaming and partially hydrating', async () => {
-    const key = createKey()
+  it.failing(
+    'should match the ssr result when streaming and partially hydrating',
+    async () => {
+      const key = createKey()
 
-    const dataDuringHydration = {}
+      const dataDuringHydration = {}
 
-    // A block fetches the data and updates the cache.
-    function Block({ suspense, delay, id }) {
-      const { data } = useSWR(key, () => createResponse('SWR', { delay }), {
-        suspense
-      })
+      // A block fetches the data and updates the cache.
+      function Block({ suspense, delay, id }) {
+        const { data } = useSWR(key, () => createResponse('SWR', { delay }), {
+          suspense
+        })
 
-      // The first render is always hydration in our case.
-      if (!dataDuringHydration[id]) {
-        dataDuringHydration[id] = data || 'undefined'
+        // The first render is always hydration in our case.
+        if (!dataDuringHydration[id]) {
+          dataDuringHydration[id] = data || 'undefined'
+        }
+
+        return <div>{data || 'undefined'}</div>
       }
 
-      return <div>{data || 'undefined'}</div>
+      // In this example, a will be hydrated first and b will still be streamed.
+      // When a is hydrated, it will update the client cache to SWR, and when
+      // b is being hydrated, it should NOT read that cache.
+      renderWithConfig(
+        <>
+          <Block id="a" suspense={false} delay={10} />
+          <Suspense fallback={null}>
+            <Block id="b" suspense={true} delay={20} />
+          </Suspense>
+        </>
+      )
+
+      // The SSR result will always be 2 undefined values because data fetching won't
+      // happen on the server:
+      //   <div>undefined</div>
+      //   <div>undefined</div>
+
+      // Wait for streaming to finish.
+      await sleep(50)
+
+      expect(dataDuringHydration).toEqual({
+        a: 'undefined',
+        b: 'undefined'
+      })
     }
-
-    // In this example, a will be hydrated first and b will still be streamed.
-    // When a is hydrated, it will update the client cache to SWR, and when
-    // b is being hydrated, it should NOT read that cache.
-    renderWithConfig(
-      <>
-        <Block id="a" suspense={false} delay={10} />
-        <Suspense fallback={null}>
-          <Block id="b" suspense={true} delay={20} />
-        </Suspense>
-      </>
-    )
-
-    // The SSR result will always be 2 undefined values because data fetching won't
-    // happen on the server:
-    //   <div>undefined</div>
-    //   <div>undefined</div>
-
-    // Wait for streaming to finish.
-    await sleep(50)
-
-    expect(dataDuringHydration).toEqual({
-      a: 'undefined',
-      b: 'undefined'
-    })
-  })
+  )
 })
