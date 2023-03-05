492ba391a020c5624788f585495a899b914e0c6e
Yixuan Xu <yixuanxu94@outlook.com>
2023-03-05 10:57:38 +0800

    test: try to fix flaky e2e test

--- e2e/test/initial-render.test.ts
+++ e2e/test/initial-render.test.ts
@@ -1,5 +1,9 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 import { test, expect } from '@playwright/test'
+
+const sleep = async (ms: number) =>
+  new Promise(resolve => setTimeout(resolve, ms))
+
 test.describe('rendering', () => {
   test('should only render once if the result of swr is not used', async ({
     page
@@ -8,6 +12,7 @@ test.describe('rendering', () => {
     await page.exposeFunction('onRender', (msg: any) => log.push(msg))
     await page.goto('./initial-render', { waitUntil: 'commit' })
     await expect(page.getByText('SWRTest')).toBeVisible()
+    await sleep(1200)
     expect(log).toHaveLength(1)
   })
 })
