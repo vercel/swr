d2b68c8ba19cb83a210fc01e7de479339ff926da
Jiachi Liu <inbox@huozhi.im>
2023-03-06 16:44:45 +0100

    update example

--- examples/subscription/pages/index.js
+++ examples/subscription/pages/index.js
@@ -1,52 +1,54 @@
-import React, { useCallback, useEffect, useState } from "react";
-import useSWRSubscription from "swr/subscription";
-import EventEmitter from "events";
+import React, { useCallback, useEffect, useState } from "react"
+import useSWRSubscription from "swr/subscription"
+import EventEmitter from "events"
 
-const event = new EventEmitter();
+const event = new EventEmitter()
 
-const swrKey = "sub-key";
+const swrKey = "sub-key"
 
 export default function page() {
-  const [isOn, setIsOn] = useState(true);
-  const [num, setNum] = useState(0);
+  const [num, setNum] = useState(0)
 
   useEffect(() => {
-    if (num % 3 === 1) {
-      const err = new Error("error:" + num);
-      event.emit("error", err);
+    if (num % 3 === 0 && num > 0) {
+      const err = new Error("error:" + num)
+      event.emit("error", err)
     } else {
-      event.emit("data", "state:" + num);
+      event.emit("data", "state:" + num)
     }
-  }, [num]);
+  }, [num])
 
   const subscribe = useCallback(
     (key, { next }) => {
-      if (!isOn) return;
-      event.on("data", (value) => next(undefined, value));
-      event.on("error", (err) => next(err));
+      const onData = (value) => next(undefined, value)
+      const onError = (err) => next(err)
+
+      event.on("data", onData)
+      event.on("error", onError)
 
       return () => {
-        setIsOn(false);
-      };
+        event.off("data", next)
+        event.off("error", next)
+      }
     },
-    [isOn]
-  );
+    []
+  )
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setNum(num + 1);
-    }, 1000);
+      setNum(num + 1)
+    }, 1000)
 
-    return () => clearInterval(timer);
-  }, [num]);
+    return () => clearInterval(timer)
+  }, [num])
 
-  const { data, error } = useSWRSubscription(swrKey, subscribe);
+  const { data, error } = useSWRSubscription(swrKey, subscribe)
   return (
     <div>
       <h3>SWR Subscription</h3>
-      <h4>Recieved every second</h4>
+      <h4>Recieved every second, error when data is times of 3</h4>
       <div>{data}</div>
       <div>{error ? error.message : ""}</div>
     </div>
-  );
+  )
 }
