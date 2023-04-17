83c9bfba3f640c972e446f1f3ab18011f238d9df
Yuta Osawa <yutaosawa81@gmail.com>
2023-04-17 21:39:00 +0900

    fix: pass serialized args to preload fetcher

--- _internal/utils/preload.ts
+++ _internal/utils/preload.ts
@@ -1,16 +1,39 @@
-import type { Middleware, Key, BareFetcher, GlobalState } from '../types'
+import type {
+  Middleware,
+  Key,
+  BareFetcher,
+  GlobalState,
+  FetcherResponse
+} from '../types'
 import { serialize } from './serialize'
 import { cache } from './config'
 import { SWRGlobalState } from './global-state'
 
-export const preload = <Data = any>(key_: Key, fetcher: BareFetcher<Data>) => {
-  const key = serialize(key_)[0]
+// Basically same as Fetcher but without Conditional Fetching
+type PreloadFetcher<
+  Data = unknown,
+  SWRKey extends Key = Key
+> = SWRKey extends () => infer Arg
+  ? (arg: Arg) => FetcherResponse<Data>
+  : SWRKey extends infer Arg
+  ? (arg: Arg) => FetcherResponse<Data>
+  : never
+
+export const preload = <
+  Data = any,
+  SWRKey extends Key = Key,
+  Fetcher extends BareFetcher = PreloadFetcher<Data, SWRKey>
+>(
+  key_: SWRKey,
+  fetcher: Fetcher
+): ReturnType<Fetcher> => {
+  const [key, fnArg] = serialize(key_)
   const [, , , PRELOAD] = SWRGlobalState.get(cache) as GlobalState
 
   // Prevent preload to be called multiple times before used.
   if (PRELOAD[key]) return PRELOAD[key]
 
-  const req = fetcher(key_)
+  const req = fetcher(fnArg) as ReturnType<Fetcher>
   PRELOAD[key] = req
   return req
 }
@@ -21,7 +44,7 @@ export const middleware: Middleware =
     const fetcher =
       fetcher_ &&
       ((...args: any[]) => {
-        const key = serialize(key_)[0]
+        const [key] = serialize(key_)
         const [, , , PRELOAD] = SWRGlobalState.get(cache) as GlobalState
         const req = PRELOAD[key]
         if (req) {
