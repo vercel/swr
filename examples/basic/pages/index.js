2d15042840eda6d39d22f0511489d92d150bf219
Shu Ding <g@shud.in>
2023-04-23 20:11:15 +0200

    revert example change

--- examples/basic/pages/index.js
+++ examples/basic/pages/index.js
@@ -1,49 +1,21 @@
-// import Link from 'next/link'
-// import fetch from '../libs/fetch'
-
-// import useSWR from 'swr'
-
-// export default function Index() {
-//   const { data } = useSWR('/api/data', fetch)
-
-//   return (
-//     <div style={{ textAlign: 'center' }}>
-//       <h1>Trending Projects</h1>
-//       <div>
-//       {
-//         data ? data.map(project =>
-//           <p key={project}><Link href='/[user]/[repo]' as={`/${project}`}>{project}</Link></p>
-//         ) : 'loading...'
-//       }
-//       </div>
-//     </div>
-//   )
-// }
+import Link from 'next/link'
+import fetch from '../libs/fetch'
 
 import useSWR from 'swr'
-import React, { useState, useEffect } from 'react'
 
-const sleep = ms =>
-  new Promise(resolve => {
-    setTimeout(() => resolve('foo'), ms)
-  })
+export default function Index() {
+  const { data } = useSWR('/api/data', fetch)
 
-export default function App() {
-  const [state, setState] = useState(false)
-  useEffect(() => {
-    let timeout = setTimeout(() => {
-      setState(true)
-    }, 2000)
-    return () => {
-      clearTimeout(timeout)
-    }
-  }, [])
-
-  const joke = useSWR('joke', () => sleep(1000))
-
-  if (!state || !joke.data) {
-    return <div>loading</div>
-  }
-
-  return <div>{joke.data}</div>
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Trending Projects</h1>
+      <div>
+      {
+        data ? data.map(project =>
+          <p key={project}><Link href='/[user]/[repo]' as={`/${project}`}>{project}</Link></p>
+        ) : 'loading...'
+      }
+      </div>
+    </div>
+  )
 }
