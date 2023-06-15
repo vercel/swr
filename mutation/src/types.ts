8ffdc15cec0248805f10ecae6b6194ba43212d92
Jiachi Liu <inbox@huozhi.im>
2023-06-16 00:33:59 +0200

    Fix mutation types order (#2669)

--- mutation/src/types.ts
+++ mutation/src/types.ts
@@ -49,7 +49,7 @@ export type SWRMutationConfiguration<
 }
 
 type RemoveUndefined<T> = T extends undefined ? never : T
-interface TriggerWithArgs<
+export interface TriggerWithArgs<
   Data = any,
   Error = any,
   SWRMutationKey extends Key = Key,
@@ -87,7 +87,7 @@ interface TriggerWithArgs<
   ): Promise<Data | undefined>
 }
 
-interface TriggerWithoutArgs<
+export interface TriggerWithoutArgs<
   Data = any,
   Error = any,
   SWRMutationKey extends Key = Key,
@@ -140,7 +140,7 @@ export interface SWRMutationResponse<
    * the fetcher, and override the options for the mutation hook.
    */
   trigger: [ExtraArg] extends [never]
-    ? TriggerWithoutArgs<Data, Error, ExtraArg, SWRMutationKey>
+    ? TriggerWithoutArgs<Data, Error, SWRMutationKey, ExtraArg>
     : TriggerWithArgs<Data, Error, SWRMutationKey, ExtraArg>
   /**
    * Function to reset the mutation state (`data`, `error`, and `isMutating`).
