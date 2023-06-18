277359da2c066803f437b8c12001ccff18339163
saengmotmi <ohjtack@gmail.com>
2023-06-18 15:03:41 +0900

    fix: revert changes

--- test/type/trigger.ts
+++ test/type/trigger.ts
@@ -95,6 +95,9 @@ export function useTestSWRMutation() {
 
   const test = () => {
     // @ts-expect-error `arg` should be 'foo'
+    trigger()
+
+    // @ts-expect-error `arg` should be 'foo'
     trigger<typeof data>('bar', {
       optimisticData: current => {
         expectType<string[] | undefined>(current)
