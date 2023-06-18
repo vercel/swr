2373a27a681849c4f2b0b031581090a5d564b9b0
Jiachi Liu <inbox@huozhi.im>
2023-06-19 00:59:28 +0200

    fix type lint

--- e2e/site/app/basic-ssr/block.tsx
+++ e2e/site/app/basic-ssr/block.tsx
@@ -1,8 +1,10 @@
 'use client'
+
 import useSWR from 'swr'
 import { useDebugHistory } from '~/lib/use-debug-history'
+
 export default function Block() {
-  const { data } = useSWR<string>('/api/data', async url => {
+  const { data } = useSWR<string>('/api/data', async (url: string) => {
     const res = await fetch(url).then(v => v.json())
     return res.name
   })
