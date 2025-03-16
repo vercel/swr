403e6dc8239c47f4e911b041bad6796df4385615
yusheng chen <samuel871211@gmail.com>
2025-03-16 19:15:41 +0800

    perf: optimize `useSWRConfig` with `useMemo` to maintain stable reference

--- src/_internal/utils/use-swr-config.ts
+++ src/_internal/utils/use-swr-config.ts
@@ -1,9 +1,14 @@
-import { useContext } from 'react'
+import { useContext, useMemo } from 'react'
 import { defaultConfig } from './config'
 import { SWRConfigContext } from './config-context'
 import { mergeObjects } from './shared'
 import type { FullConfiguration } from '../types'
 
 export const useSWRConfig = (): FullConfiguration => {
-  return mergeObjects(defaultConfig, useContext(SWRConfigContext))
+  const parentConfig = useContext(SWRConfigContext)
+  const mergedConfig = useMemo(
+    () => mergeObjects(defaultConfig, parentConfig),
+    [parentConfig]
+  )
+  return mergedConfig
 }
