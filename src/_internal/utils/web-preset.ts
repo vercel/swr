c5a43892a52c645b9eeeced64cddc43bbff2604e
David Condrey <davidcondrey@me.com>
2025-04-17 05:58:09 -0700

    Update web-preset.ts
    Enhanced visibility check.  Resolves #3039

--- src/_internal/utils/web-preset.ts
+++ src/_internal/utils/web-preset.ts
@@ -22,8 +22,18 @@ const [onWindowEvent, offWindowEvent] =
     : [noop, noop]
 
 const isVisible = () => {
-  const visibilityState = isDocumentDefined && document.visibilityState
-  return isUndefined(visibilityState) || visibilityState !== 'hidden'
+  if (!isDocumentDefined) return true
+
+  try {
+    const isDocVisible = document.visibilityState !== 'hidden'
+    
+    const isDocFocused =
+      typeof document.hasFocus === 'function' ? document.hasFocus() : true
+    
+    return isDocVisible && isDocFocused
+  } catch (err) {
+    return true
+  }
 }
 
 const initFocus = (callback: () => void) => {
