311852132f987bf40746f0a8e3b6145f1b62af6b
Jiachi Liu <inbox@huozhi.im>
2025-07-29 12:20:17 +0200

    Revert "feat: modify cache type to allow generic usage" (#4151)

--- src/_internal/types.ts
+++ src/_internal/types.ts
@@ -518,8 +518,8 @@ export type RevalidateCallback = <K extends RevalidateEvent>(
 
 export interface Cache<Data = any> {
   keys(): IterableIterator<string>
-  get<T = Data>(key: string): State<T> | undefined
-  set<T = Data>(key: string, value: State<T>): void
+  get(key: string): State<Data> | undefined
+  set(key: string, value: State<Data>): void
   delete(key: string): void
 }
 
