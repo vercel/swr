8288eb9eee94eb6d67089ed3eb184d3097b04fa3
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-18 12:13:05 -0800

    Add i18n to search

--- docs/app/api/search/route.ts
+++ docs/app/api/search/route.ts
@@ -1,7 +1,16 @@
-import { createFromSource } from "fumadocs-core/search/server";
-import { source } from "@/lib/geistdocs/source";
+import { createFromSource } from 'fumadocs-core/search/server'
+import { source } from '@/lib/geistdocs/source'
 
 export const { GET } = createFromSource(source, {
   // https://docs.orama.com/docs/orama-js/supported-languages
-  language: "english",
-});
+  language: 'english',
+  localeMap: {
+    'en-US': { language: 'english' },
+    'fr-FR': { language: 'french' },
+    'ja-FR': { language: 'japanese' },
+    'pt-BR': { language: 'portuguese' },
+    'zh-CN': { language: 'chinese' },
+    ko: { language: 'korean' },
+    ru: { language: 'russian' }
+  }
+})
