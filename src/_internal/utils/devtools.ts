9bc2fe8c7351ac56a7ce1c0ebacc76491e1d43aa
SukkaW <isukkaw@gmail.com>
2026-05-21 02:45:39 +0800

    feat: expose callback for swr devtools extension

--- src/_internal/utils/devtools.ts
+++ src/_internal/utils/devtools.ts
@@ -15,3 +15,14 @@ export const setupDevTools = () => {
     window.__SWR_DEVTOOLS_REACT__ = React
   }
 }
+
+// Due to Chrome extension limitations, the SWR DevTools extension may inject
+// `__SWR_DEVTOOLS_USE__` too late, where `enableDevtools` will always be false
+// In this case, we provide a global function `__SWR_DEVTOOLS_SETUP__` for the
+// extension to invoke when it is ready
+
+// @ts-expect-error
+window.__SWR_DEVTOOLS_SETUP__ = () => {
+  // @ts-expect-error
+  window.__SWR_DEVTOOLS_REACT__ = React
+}
