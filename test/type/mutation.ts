df640aee54d18b6d482bbdf61f208234417590fd
joaquimpeixoto <jhcpeixoto@outlook.pt>
2026-05-27 15:18:13 +0100

    fix: keep mutation trigger callable with generics

--- test/type/mutation.ts
+++ test/type/mutation.ts
@@ -1,7 +1,20 @@
-import useSWRMutation, { type TriggerWithoutArgs } from 'swr/mutation'
+import useSWRMutation, {
+  type MutationFetcher,
+  type TriggerWithoutArgs
+} from 'swr/mutation'
 import { expectType } from './utils'
 
 export function useConfigMutation() {
   const { trigger } = useSWRMutation('key', k => k)
   expectType<TriggerWithoutArgs<'key', any, string, never>>(trigger)
+}
+
+export function useGenericMutation<
+  Key extends string = string,
+  Data = unknown,
+  ExtraArg = unknown
+>(key: Key, fetcher: MutationFetcher<Data, Key, ExtraArg>) {
+  const { trigger } = useSWRMutation(key, fetcher)
+
+  trigger({ someArg: 'value' } as ExtraArg)
 }
